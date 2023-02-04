1c4162005f654126af49b28b17facee51c875301
alexb <alexandrbirichevskiy@gmail.com>
2023-02-04 12:13:16 +0300

    #2 Add Hilt

---
+++ app/src/main/java/ru/alexandrbirichevskiy/mykinopoiskfintech/app/App.kt
@@ -0,0 +1,7 @@
+package ru.alexandrbirichevskiy.mykinopoiskfintech.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- app/src/main/java/ru/alexandrbirichevskiy/mykinopoiskfintech/presentation/MainActivity.kt
+++ app/src/main/java/ru/alexandrbirichevskiy/mykinopoiskfintech/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package ru.alexandrbirichevskiy.mykinopoiskfintech
+package ru.alexandrbirichevskiy.mykinopoiskfintech.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import ru.alexandrbirichevskiy.mykinopoiskfintech.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
