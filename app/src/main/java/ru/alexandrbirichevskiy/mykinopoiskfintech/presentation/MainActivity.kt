e8e69f2e359bf810c4536eb9d470ee012a2bf229
alexandrbirichevskiy <77644368+alexandrbirichevskiy@users.noreply.github.com>
2023-02-04 18:23:17 +0300

    Add Navigation (#3)
    * #2 Add Hilt
    
    * Task #3 Add navigation dependencies
    
    * Task #3 Add compose dependencies and screen-composable files
    
    * Task #3 Add Navigation

--- app/src/main/java/ru/alexandrbirichevskiy/mykinopoiskfintech/presentation/MainActivity.kt
+++ app/src/main/java/ru/alexandrbirichevskiy/mykinopoiskfintech/presentation/MainActivity.kt
@@ -2,13 +2,18 @@ package ru.alexandrbirichevskiy.mykinopoiskfintech.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
 import ru.alexandrbirichevskiy.mykinopoiskfintech.R
+import ru.alexandrbirichevskiy.mykinopoiskfintech.presentation.navigation.Navigation
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            Navigation()
+        }
     }
 }
