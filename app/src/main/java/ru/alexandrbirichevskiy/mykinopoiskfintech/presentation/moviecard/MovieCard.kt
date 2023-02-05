7df6392fe9a5806a70e5ac88e18e064de7b2b6bb
alexb <alexandrbirichevskiy@gmail.com>
2023-02-05 19:52:04 +0300

    Task#7 Add BackPressed Button

--- app/src/main/java/ru/alexandrbirichevskiy/mykinopoiskfintech/presentation/moviecard/MovieCard.kt
+++ app/src/main/java/ru/alexandrbirichevskiy/mykinopoiskfintech/presentation/moviecard/MovieCard.kt
@@ -1,8 +1,7 @@
 package ru.alexandrbirichevskiy.mykinopoiskfintech.presentation.moviecard
 
-import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
+import androidx.activity.compose.BackHandler
+import androidx.compose.animation.VectorConverter
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,24 +11,32 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlurEffect
+import androidx.compose.ui.focus.FocusRequester.Companion.createRefs
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import ru.alexandrbirichevskiy.mykinopoiskfintech.R
+import ru.alexandrbirichevskiy.mykinopoiskfintech.presentation.navigation.Screens
 
 @Composable
 fun MovieCard(
@@ -46,6 +53,10 @@ fun MovieCard(
         systemUiController.setSystemBarsColor(
             color = Color.Transparent
         )
+    }
+
+    BackHandler {
+        controller.navigate(Screens.PopularMovies.route)
     }
 
     Column(
@@ -113,6 +124,25 @@ fun MovieCard(
                 )
             }
             Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
+
+    ConstraintLayout {
+        IconButton(
+            onClick = { controller.navigate(Screens.PopularMovies.route) },
+            modifier = Modifier
+                .size(24.dp)
+                .constrainAs(createRef()) {
+                    top.linkTo(parent.top, margin = 30.dp)
+                    start.linkTo(parent.start, margin = 22.dp)
+                }
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.arrow_back),
+                modifier = Modifier.fillMaxSize(),
+                contentDescription = null,
+                tint = colorResource(id = R.color.blue)
+            )
         }
     }
 }
