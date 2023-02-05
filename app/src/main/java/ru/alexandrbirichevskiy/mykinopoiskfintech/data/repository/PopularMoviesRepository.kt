3c6c0f00b3f6c81329c685954894804e439586c0
alexandrbirichevskiy <77644368+alexandrbirichevskiy@users.noreply.github.com>
2023-02-05 15:36:47 +0300

    Add Paging (#6)
    * #1 Create a project
    
    * #2 Add Hilt
    
    * Add Hilt (#2)
    
    * Task #3 Add navigation dependencies
    
    * Task #3 Add compose dependencies and screen-composable files
    
    * Task #3 Add Navigation
    
    * Add Navigation (#3)
    
    * #2 Add Hilt
    
    * Task #3 Add navigation dependencies
    
    * Task #3 Add compose dependencies and screen-composable files
    
    * Task #3 Add Navigation
    
    * Task#4 Add Retrofit and Moshi dependencies
    
    * Task#4 Add usecase and repo
    
    * Task#4 Add Pupular Movies and architecture project
    
    * #4 Add Pupular Movies and architecture project (#4)
    
    * #2 Add Hilt
    
    * Task #3 Add navigation dependencies
    
    * Task #3 Add compose dependencies and screen-composable files
    
    * Task #3 Add Navigation
    
    * Task#4 Add Retrofit and Moshi dependencies
    
    * Task#4 Add usecase and repo
    
    * Task#4 Add Pupular Movies and architecture project
    
    * Task#5 Add TokenInterceptor
    
    * Add TokenInterceptor (#5)
    
    * #2 Add Hilt
    
    * Task #3 Add navigation dependencies
    
    * Task #3 Add compose dependencies and screen-composable files
    
    * Task #3 Add Navigation
    
    * Task#4 Add Retrofit and Moshi dependencies
    
    * Task#4 Add usecase and repo
    
    * Task#4 Add Pupular Movies and architecture project
    
    * Task#5 Add TokenInterceptor
    
    * Task#6 Add Paging

--- app/src/main/java/ru/alexandrbirichevskiy/mykinopoiskfintech/data/repository/PopularMoviesRepository.kt
+++ app/src/main/java/ru/alexandrbirichevskiy/mykinopoiskfintech/data/repository/PopularMoviesRepository.kt
@@ -1,18 +1,39 @@
 package ru.alexandrbirichevskiy.mykinopoiskfintech.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import ru.alexandrbirichevskiy.mykinopoiskfintech.data.models.MovieModel
 import ru.alexandrbirichevskiy.mykinopoiskfintech.data.models.PopularMoviesModel
 import ru.alexandrbirichevskiy.mykinopoiskfintech.domain.network.PopularMoviesApi
+import ru.alexandrbirichevskiy.mykinopoiskfintech.domain.network.PopularMoviesPagingSource
+import ru.alexandrbirichevskiy.mykinopoiskfintech.domain.responses.FilmResponse
 import ru.alexandrbirichevskiy.mykinopoiskfintech.extensions.toModelMapper
 
+
 interface PopularMoviesRepository {
-    suspend fun getPopularMovies(): PopularMoviesModel
+    fun getPopularMovies(): Flow<PagingData<MovieModel>>
 }
 
 class PopularMoviesRepositoryImpl(
-    private val popularMoviesApi: PopularMoviesApi
+    private val api: PopularMoviesApi
 ) : PopularMoviesRepository {
 
-    override suspend fun getPopularMovies(): PopularMoviesModel {
-        return popularMoviesApi.getPopularMovies().body()!!.toModelMapper()
+    override fun getPopularMovies(): Flow<PagingData<MovieModel>> {
+        return Pager(
+            config = PagingConfig(
+                pageSize = 20
+            ),
+            pagingSourceFactory = {
+                PopularMoviesPagingSource(api = api)
+            }
+        ).flow.map {
+            it.map { data ->
+                data.toModelMapper()
+            }
+        }
     }
 }
--- app/src/main/java/ru/alexandrbirichevskiy/mykinopoiskfintech/domain/usecases/PopularMoviesUseCase.kt
+++ app/src/main/java/ru/alexandrbirichevskiy/mykinopoiskfintech/domain/usecases/PopularMoviesUseCase.kt
@@ -1,16 +1,19 @@
 package ru.alexandrbirichevskiy.mykinopoiskfintech.domain.usecases
 
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+import ru.alexandrbirichevskiy.mykinopoiskfintech.data.models.MovieModel
 import ru.alexandrbirichevskiy.mykinopoiskfintech.data.models.PopularMoviesModel
 import ru.alexandrbirichevskiy.mykinopoiskfintech.data.repository.PopularMoviesRepository
 
 interface PopularMoviesUseCase {
-    suspend fun getPopularMovies(): PopularMoviesModel
+    fun getPopularMovies(): Flow<PagingData<MovieModel>>
 }
 
 class PopularMoviesUseCaseImpl(
     private val popularMoviesRepository: PopularMoviesRepository
 ) : PopularMoviesUseCase {
-    override suspend fun getPopularMovies(): PopularMoviesModel {
+    override fun getPopularMovies(): Flow<PagingData<MovieModel>> {
         return popularMoviesRepository.getPopularMovies()
     }
 }
